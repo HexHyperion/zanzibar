55fbb2552c643635e6e75cb51ed5de2d86b48c47
Szymon <szymonjan@gmail.com>
2025-01-13 13:55:06 +0100

    Fix password generation

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,6 @@
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
     let maxLength = 0;
-    let mainWord = "";
 
     async function fetchData() {
         const fetchedWord = await fetch(wordEndpoint);
@@ -77,16 +76,17 @@
     
     
     function checkPasswordIncluded(letterCounts: Map<string,number>, password: string) {
+        let isOK = true;
         const passwordLetterCounts = new Map<string, number>();
         for (let i = 0; i < password.length; i++) {
             passwordLetterCounts.set(password[i], (passwordLetterCounts.get(password[i]) ?? 0) + 1);
         }
         passwordLetterCounts.forEach((value, key) => {
             if ((passwordLetterCounts.get(key) ?? 0) > (letterCounts.get(key) ?? 0)) {
-                return false;
+                isOK = false;
             }
         });
-        return true;
+        return isOK;
     }
 
 
@@ -188,6 +188,15 @@
     }
 
 
+    function stripDefinition(array: Array<Array<string>>) {
+        let returnArray = [] as Array<string>;
+        array.forEach(wordWithDef => {
+            returnArray.push(wordWithDef[0]);
+        });
+        return returnArray;
+    }
+
+
     onMount(() => {
         const buttons = document.querySelectorAll(".alphabet-button") as NodeListOf<HTMLButtonElement>;
         buttons.forEach(button => {
@@ -203,86 +212,87 @@
         <h1 class="text-white">Loading...</h1>
     </div>
 {:then words}
-    <div class="box-border flex items-start justify-center flex-col mb-4 gap-1 w-fit">
-        {#each words as word, index}
-            <div class="flex gap-1 items-center">
-                <p class="text-white text-xl mr-3 w-5">{index+1}.</p>
-                {#each word[0] as letter}
-                    <input class="w-10 h-10 text-center text-2xl bg-neutral-900 crossword-input"
-                        type="text"
-                        maxlength="1"
-                        autocapitalize="off"
-                        autocorrect="off"
-                        autocomplete="off"
-                        spellcheck="false"
-                        data-letter={letter.toUpperCase()}
-                        on:input={(e) => {
-                            ((e.target as HTMLInputElement).nextElementSibling as HTMLInputElement)?.focus();
-                            checkAllLettersVisible();
-                        }}
-                        on:keydown={(e) => {
-                            // Gotta love TypeScript inside HTML inside Svelte
-                            if (e.key == "Backspace" && !(e.target as HTMLInputElement).readOnly) {
-                                (e.target as HTMLInputElement).value = "";
-                                (e.target as HTMLInputElement).focus();
-                            }
-                            else if (e.key == "Enter"
-                                && e.target
-                                && checkFilledRow(e.target as HTMLInputElement)
-                            ) {
-                                if (checkCorrectRow(e.target as HTMLInputElement)) {
-                                    setSuccess(e.target as HTMLInputElement);
-                                }
-                                ((e.target as HTMLInputElement).parentElement?.nextElementSibling?.children[1] as HTMLInputElement)?.focus();
-                            }
-                            else if (e.key == "ArrowLeft") {
-                                ((e.target as HTMLInputElement).previousElementSibling as HTMLInputElement)?.focus();
-                            }
-                            else if (e.key == "ArrowRight") {
+    {#await generatePassword(stripDefinition(words)) then password}
+        <div class="box-border flex items-start justify-center flex-col mb-4 gap-1 w-fit">
+            {#each words as word, index}
+                <div class="flex gap-1 items-center">
+                    <p class="text-white text-xl mr-3 w-5">{index+1}.</p>
+                    {#each word[0] as letter}
+                        <input class="w-10 h-10 text-center text-2xl bg-neutral-900 crossword-input"
+                            type="text"
+                            maxlength="1"
+                            autocapitalize="off"
+                            autocorrect="off"
+                            autocomplete="off"
+                            spellcheck="false"
+                            data-letter={letter.toUpperCase()}
+                            on:input={(e) => {
                                 ((e.target as HTMLInputElement).nextElementSibling as HTMLInputElement)?.focus();
-                            }
-                            else if (e.key == "ArrowUp") {
-                                ((e.target as HTMLInputElement).parentElement?.previousElementSibling?.children[1] as HTMLInputElement)?.focus();
-                            }
-                            else if (e.key == "ArrowDown") {
-                                ((e.target as HTMLInputElement).parentElement?.nextElementSibling?.children[1] as HTMLInputElement)?.focus();
-                            }
-                            checkAllLettersVisible();
-                        }}
-                    >
-                {/each}
-                {#each Array(maxLength - word[0].length) as _}
-                    <div class="w-10 h-10 bg-neutral-950"></div>
-                {/each}
-            </div>
-        {/each}
-    </div>
-    <div class="box-border flex justify-center items-center mb-4 gap-1 h-fit">
-        <div class="flex gap-1 items-center">
-            <!-- For now, small test -->
-            {#each Array(9) as _, i}
-                <div class="relative">
-                    <input class="w-10 h-10 text-center text-2xl bg-neutral-900 password-input" type="text" maxlength="1" readonly>
-                    <span class="absolute bottom-1 left-1 text-xs text-neutral-400">{i + 1}</span>
+                                checkAllLettersVisible();
+                            }}
+                            on:keydown={(e) => {
+                                // Gotta love TypeScript inside HTML inside Svelte
+                                if (e.key == "Backspace" && !(e.target as HTMLInputElement).readOnly) {
+                                    (e.target as HTMLInputElement).value = "";
+                                    (e.target as HTMLInputElement).focus();
+                                }
+                                else if (e.key == "Enter"
+                                    && e.target
+                                    && checkFilledRow(e.target as HTMLInputElement)
+                                ) {
+                                    if (checkCorrectRow(e.target as HTMLInputElement)) {
+                                        setSuccess(e.target as HTMLInputElement);
+                                    }
+                                    ((e.target as HTMLInputElement).parentElement?.nextElementSibling?.children[1] as HTMLInputElement)?.focus();
+                                }
+                                else if (e.key == "ArrowLeft") {
+                                    ((e.target as HTMLInputElement).previousElementSibling as HTMLInputElement)?.focus();
+                                }
+                                else if (e.key == "ArrowRight") {
+                                    ((e.target as HTMLInputElement).nextElementSibling as HTMLInputElement)?.focus();
+                                }
+                                else if (e.key == "ArrowUp") {
+                                    ((e.target as HTMLInputElement).parentElement?.previousElementSibling?.children[1] as HTMLInputElement)?.focus();
+                                }
+                                else if (e.key == "ArrowDown") {
+                                    ((e.target as HTMLInputElement).parentElement?.nextElementSibling?.children[1] as HTMLInputElement)?.focus();
+                                }
+                                checkAllLettersVisible();
+                            }}
+                        >
+                    {/each}
+                    {#each Array(maxLength - word[0].length) as _}
+                        <div class="w-10 h-10 bg-neutral-950"></div>
+                    {/each}
                 </div>
             {/each}
         </div>
-    </div>
-    <div class="box-border flex justify-center items-center mb-4 gap-1 h-fit flex-wrap">
+        <div class="box-border flex justify-center items-center mb-4 gap-1 h-fit">
+            <div class="flex gap-1 items-center">
+                {#each password as letter, i}
+                    <div class="relative">
+                        <!-- The placeholder is temporary -->
+                        <input class="w-10 h-10 text-center text-2xl bg-neutral-900 password-input" type="text" maxlength="1" data-letter="{letter}" readonly placeholder="{letter.toUpperCase()}">
+                        <span class="absolute bottom-0 left-0 h-fit text-xs text-neutral-400">{i+1}</span>
+                    </div>
+                {/each}
+            </div>
+        </div>
+        <div class="box-border flex justify-center items-center mb-4 gap-1 h-fit flex-wrap">
+            {#each alphabet as letter}
+                <button id="button-letter-{letter}" class="w-6 h-8 bg-violet-500 text-white alphabet-button" on:click={(event) => letterPress(letter, event.currentTarget as HTMLButtonElement)}><p class="w-full h-full flex justify-center items-center text-center">{letter}</p></button>
+            {/each}
+        </div>
+        <div class="box-border flex justify-center items-start flex-col gap-1">
+            {#each words as wordDef, index}
+                <p class="text-white text-left">{index+1}. {wordDef[1]}</p>
+            {/each}
+        </div>
         {#each alphabet as letter}
-            <button id="button-letter-{letter}" class="w-6 h-8 bg-violet-500 text-white alphabet-button" on:click={(event) => letterPress(letter, event.currentTarget as HTMLButtonElement)}><p class="w-full h-full flex justify-center items-center text-center">{letter}</p></button>
+            <!-- That's cool -->
+            {#await tick()}
+                {checkLetterExisting(document.getElementById(`button-letter-${letter}`) as HTMLButtonElement, letter)}
+            {/await}
         {/each}
-    </div>
-    <div class="box-border flex justify-center items-start flex-col gap-1">
-        {#each words as wordDef, index}
-            <p class="text-white text-left">{index+1}. {wordDef[1]}</p>
-        {/each}
-    </div>
-    {#each alphabet as letter}
-        <!-- That's cool -->
-        {#await tick()}
-            {checkLetterExisting(document.getElementById(`button-letter-${letter}`) as HTMLButtonElement, letter)}
-            
-        {/await}
-    {/each}
+    {/await}
 {/await}
